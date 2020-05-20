5d87c3d10521377b93b419c297df0cd7ef3dcb2b
gaox <gx_zone@126.com>
2020-05-20 22:52:21 +0800

    [dev] 不占用端口方式启动
    完全关闭HTTP端点，但仍创建一个WebApplicationContext

---
+++ occupation/src/main/java/net/gaox/occupation/OccupationApplication.java
@@ -0,0 +1,18 @@
+package net.gaox.occupation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> 占用大量端口实现 </p>
+ *
+ * @author gaox·Eric
+ * @date 2020/5/20 21:32
+ */
+@SpringBootApplication
+public class OccupationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OccupationApplication.class, args);
+    }
+}
---
+++ occupation/src/test/java/net/gaox/occupation/OccupationApplicationTests.java
@@ -0,0 +1,16 @@
+package net.gaox.occupation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author gaox·Eric
+ * @date 2020/5/20 21:32
+ */
+@SpringBootTest
+class OccupationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
